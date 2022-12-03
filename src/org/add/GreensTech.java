2666c6dcd772ee449b28f0767e087deaf3875828
Maniraj498 <manirajendran498@gmail.com>
2022-12-03 19:51:33 +0530

    greens

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,30 @@
+package org.add;
+
+public class GreensTech {
+private void greensOmr() {
+	System.out.println("OMR");
+
+}
+private void greensAdayar() {
+System.out.println("ADAYAR");
+
+}
+private void greensTambaram() {
+System.out.println("TAMBRAM");
+
+}
+private void greensVelacherry() {
+System.out.println("VELACHERRY");
+}
+private void greensAnnaNagar() {
+System.out.println("ANNANAGAR");
+} 
+public static void main(String[] args) {
+	GreensTech c=new GreensTech();
+	c.greensOmr();
+	c.greensAdayar();
+	c.greensTambaram();
+	c.greensVelacherry();
+	c.greensAnnaNagar();
+}
+}
